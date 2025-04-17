bd5f1fb5b93dcb55c336e40939e991cfe0a3dfaf
mfu96 <furkan42uslu@gmail.com>
2025-04-17 17:42:58 +0300

    170425

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-    apiUrl:'https://localhost:44330/api/',
-  baseUrl:'https://localhost:44330'
+  apiUrl: 'https://localhost:44330/api/',
+  baseUrl: 'https://localhost:44330'
 };
